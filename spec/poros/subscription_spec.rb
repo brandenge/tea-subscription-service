0ebb51f719bda8b47bc1c720c21e1823850d47fa
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-07-27 01:50:29 -0500

    Fix: tests for Subscription PORO

--- spec/poros/subscription_spec.rb
+++ spec/poros/subscription_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :poro do
-  attributes = {
-    id: 123,
-    title: 'Subscription for tea',
-    price: 500,
-    status: 'Active',
-    frequncy: 10
-  }
+  subject(:subscription) do
+    @customer = FactoryBot.create(:customer)
+    @tea = FactoryBot.create(:tea)
 
-  subject(:subscription) { create(:subscription, attributes) }
+    attributes = {
+      id: 123,
+      customer_id: @customer.id,
+      tea_id: @tea.id,
+      title: 'Subscription for tea',
+      price: 500,
+      status: 'Active',
+      frequency: 10
+    }
+    create(:subscription, attributes)
+  end
 
   describe '#initialize' do
     it 'exists' do
@@ -20,6 +26,8 @@ RSpec.describe Subscription, type: :poro do
   describe 'attributes' do
     it 'has attributes' do
       expect(subscription.id).to eq(123)
+      expect(subscription.customer_id).to eq(@customer.id)
+      expect(subscription.tea_id).to eq(@tea.id)
       expect(subscription.title).to eq('Subscription for tea')
       expect(subscription.price).to eq(500)
       expect(subscription.status).to eq('Active')
