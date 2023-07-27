d0a3628b0f64e77e77e1336b6e92e4d7fc00d5f1
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-07-27 01:01:17 -0500

    Feat: add factories and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+customer1 = FactoryBot.create(:customer)
+customer2 = FactoryBot.create(:customer)
+customer3 = FactoryBot.create(:customer)
+customer4 = FactoryBot.create(:customer)
+customer5 = FactoryBot.create(:customer)
+
+tea1 = FactoryBot.create(:tea)
+tea2 = FactoryBot.create(:tea)
+tea3 = FactoryBot.create(:tea)
+tea4 = FactoryBot.create(:tea)
+tea5 = FactoryBot.create(:tea)
+
+subscription1 = FactoryBot.create(:subscription, customer: customer1, tea: tea1)
+subscription1 = FactoryBot.create(:subscription, customer: customer1, tea: tea2)
+subscription1 = FactoryBot.create(:subscription, customer: customer2, tea: tea2)
+subscription1 = FactoryBot.create(:subscription, customer: customer3, tea: tea3)
+subscription1 = FactoryBot.create(:subscription, customer: customer4, tea: tea4)
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,8 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'factory_bot'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -37,6 +39,7 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
